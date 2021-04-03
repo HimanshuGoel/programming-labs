b370995bef2790eb4b8e98a901b20532443e4d0e
Himanshu Goel <himanshu.goel@soprabanking.com>
2021-04-03 15:27:01 +0530

    add nodemon

--- backend2/index.js
+++ backend2/index.js
@@ -1,1 +1,13 @@
-var name = 'himanshu'
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('welcome to my api!')
+});
+
+app.listen(port, () => {
+    console.log('running on port ' + port)
+});
